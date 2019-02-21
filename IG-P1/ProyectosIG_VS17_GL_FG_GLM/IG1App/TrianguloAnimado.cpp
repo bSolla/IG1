965b14d4d478834c326fe16a311b4d881929cbab
Belén Solla <bsolla@ucm.es>
2019-02-21 17:48:02 +0100

    Fixed Gitignore and finished animated triangle

--- IG-P1/ProyectosIG_VS17_GL_FG_GLM/IG1App/TrianguloAnimado.cpp
+++ IG-P1/ProyectosIG_VS17_GL_FG_GLM/IG1App/TrianguloAnimado.cpp
@@ -19,11 +19,22 @@ void TrianguloAnimado::render(Camera const & cam)
 	if (mesh != nullptr) {
 		uploadMvM(cam.getViewMat());
 		glLineWidth(2);
+
+		x = radio * glm::cos(glm::radians(ang));
+		y = radio * glm::sin(glm::radians(ang));
+
+		modelMat = translate(modelMat, glm::dvec3(x, y, 0.0));
+		ang += incrAng;
+
 		mesh->render();
 	}
 }
 
 void TrianguloAnimado::update()
 {
-	modelMat = rotate(modelMat, glm::radians(25.0), glm::dvec3(0.0, 0.0, 1.0));
+	glm::dmat4 triangleMat = getModelMat();
+	glm::dmat4 matR;
+	matR = rotate(glm::dmat4(1), glm::radians(-25.0), glm::dvec3(0.0, 0.0, 1.0));
+
+	setModelMat(triangleMat * matR);
 }
--- IG-P1/ProyectosIG_VS17_GL_FG_GLM/IG1App/TrianguloAnimado.h
+++ IG-P1/ProyectosIG_VS17_GL_FG_GLM/IG1App/TrianguloAnimado.h
@@ -3,6 +3,12 @@
 class TrianguloAnimado :
 	public Entity
 {
+private:
+	double ang = 90.0;
+	double incrAng = 1.0;
+	double radio = 100.0;
+	double x, y;
+
 public:
 	TrianguloAnimado(GLdouble r);
 	virtual ~TrianguloAnimado();
