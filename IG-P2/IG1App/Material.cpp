435a0c72d096fc9c6bcd999226f2911bc1ee55d2
Belén Solla <bsolla@ucm.es>
2019-04-23 05:56:48 +0200

    Added Material class

---
+++ IG-P2/IG1App/Material.cpp
@@ -0,0 +1,43 @@
+#include "Material.h"
+
+
+
+Material::Material () {
+	setBlackPlastic (); // default material
+}
+
+
+Material::~Material () {
+}
+
+void Material::upload () {
+}
+
+
+void Material::setBlackPlastic () {
+	expF = 32.0f;
+	ambient = glm::fvec4 (0.0f, 0.0f, 0.0f, expF);
+	diffuse = glm::fvec4 (0.01f, 0.01f, 0.01f, expF);
+	specular = glm::fvec4 (0.5f, 0.5f, 0.5f, expF);
+}
+
+void Material::setBrass () {
+	expF = 27.8974f;
+	ambient = glm::fvec4 (0.329412f, 0.223529f, 0.027451f, expF);
+	diffuse = glm::fvec4 (0.780392f, 0.568627f, 0.113725f, expF);
+	specular = glm::fvec4 (0.992157f, 0.941176f, 0.907843f, expF);
+}
+
+void Material::setGold () {
+	expF = 51.2f;
+	ambient = glm::fvec4 (0.24725f, 0.1995f, 0.0745f, expF);
+	diffuse = glm::fvec4 (0.75164f, 0.60648f, 0.22648f, expF);
+	specular = glm::fvec4 (0.628281f, 0.555802f, 0.366065f, expF);
+}
+
+void Material::setPewter () {
+	expF = 9.84615f;
+	ambient = glm::fvec4 (0.10588f, 0.058824f, 0.113725f, expF);
+	diffuse = glm::fvec4 (0.427451f, 0.470588f, 0.541176f, expF);
+	specular = glm::fvec4 (0.3333f, 0.3333f, 0.521569f, expF);
+}
---
+++ IG-P2/IG1App/Material.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <GL/freeglut.h>
+#include <glm.hpp>
+
+class Material {
+public:
+	Material ();
+	~Material ();
+
+	virtual void upload ();
+	void setBlackPlastic ();
+	void setBrass ();
+	void setGold ();
+	void setPewter ();
+
+private:
+	glm::fvec4 ambient, diffuse, specular; // coeficientes de reflexión
+	GLfloat expF; // exponente especular
+	GLuint face = GL_FRONT_AND_BACK; // ambos lados
+	GLuint sh = GL_SMOOTH; // smooth / flat shading
+};
+
